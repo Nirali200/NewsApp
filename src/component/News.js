2ed99cfde86efdfa3e9b1a43089b3ab9a58293dd
Nirali200 <darjinirali08@gmail.com>
2023-08-20 22:26:12 +0530

    Add states

--- src/component/News.js
+++ src/component/News.js
@@ -8,13 +8,13 @@ export default class News extends Component {
         <h2>NewsApp - Top headlines</h2>
         <div className='row'>
         <div className='col-md-4'>
-        <NewsItem title="myTitle" description="myDsc"/>
+        <NewsItem title="myTitle" description="myDsc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
         </div>
         <div className='col-md-4'>
-        <NewsItem title="myTitle" description="myDsc"/>
+        <NewsItem title="myTitle" description="myDsc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
         </div>
         <div className='col-md-4'>
-        <NewsItem title="myTitle" description="myDsc"/>
+        <NewsItem title="myTitle" description="myDsc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
         </div>
         </div>
       </div>
