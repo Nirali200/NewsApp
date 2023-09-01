32ad3959dde5f04fc1e07908450a26b79b9aedbd
Nirali200 <darjinirali08@gmail.com>
2023-09-01 11:55:03 +0530

    Author and Date added

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -15,22 +15,20 @@ export class navbar extends Component {
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a></li>
           <li className="nav-item">
-          <a className="nav-link" href="/"> business</a></li>
+          <a className="nav-link" href="/business">Business</a></li>
           <li className="nav-item">
-          <a className="nav-link" href="/">entertainment</a></li>
+          <a className="nav-link" href="/entertainment">Entertainment</a></li>
           <li className="nav-item">
-          <a className="nav-link" href="/">general</a></li>
+          <a className="nav-link" href="/general">General</a></li>
           <li className="nav-item">
-          <a className="nav-link" href="/">health</a></li>
+          <a className="nav-link" href="/health">Health</a></li>
           <li className="nav-item">
-          <a className="nav-link" href="/">science</a></li>
+          <a className="nav-link" href="/science">Science</a></li>
           <li className="nav-item">
-          <a className="nav-link" href="/">sports</a></li>
+          <a className="nav-link" href="/sports">Sports</a></li>
           <li className="nav-item">
-          <a className="nav-link" href="/">technology</a></li>
+          <a className="nav-link" href="/technology">Technology</a></li>
         
       </ul>
     </div>
