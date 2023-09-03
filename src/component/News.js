9c9823c94981ffd921ac2849c0a0f0b0e18d14ff
Nirali200 <darjinirali08@gmail.com>
2023-09-03 11:50:05 +0530

    Infinite scrollbar added

--- src/component/News.js
+++ src/component/News.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export default class News extends Component {
 
@@ -8,7 +10,8 @@ super()
 this.state = {
 articles:[],
 loading:false,
-page:1}
+page:1,
+totalResults:0}
 }
 async componentDidMount(){
       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e5a1cd408f8948c79b50b900baccb545&page=1&pageSize=${this.props.pageSize}`;
@@ -30,33 +33,54 @@ handlePre = async () =>{
 
 }
 handleNext =async () =>{
-  console.log("Next")
   
   let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e5a1cd408f8948c79b50b900baccb545&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       let data = await fetch(url);
       let parseData = await data.json();
-      console.log(data);
       this.setState({
     page: this.state.page + 1,
     articles:parseData.articles
   })
 }
+
+
+fetchMoreData = async () => {
+  this.setState({page: this.state.page+1})
+  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e5a1cd408f8948c79b50b900baccb545&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      let parseData = await data.json();
+      this.setState({
+    page: this.state.page + 1,
+    articles:this.state.articles.concat(parseData.articles)
+  })
+};
+
+
+
   render() {
     return (
       <div className='container my-3'>
         <h2>NewsApp - Top headlines</h2>
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<h4>Loading...</h4>}
+        >
+          
+        <div className='container'>
         <div className='row'>
         {this.state.articles.map((element)=>{
          return <div className='col-md-4' key={element.url}>
         <NewsItem  title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
         </div>
         })}
-        <div className="container d-flex justify-content-between">
-        <button disabled={this.state.page<=1} type="button" class="btn btn-primary" onClick={this.handlePre}> &larr; Previous</button>
-        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" class="btn btn-primary" onClick={this.handleNext}>Next &rarr;</button>
         </div>
         </div>
-      </div>
+        </InfiniteScroll>
+        
+        </div>
     )
   }
 }
